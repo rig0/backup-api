c14cbb97ef72b13afbeb003639fdcec979cc75d0
Rigo <rigo-dev@pm.me>
2025-11-26 16:29:51 -0500

    .venv

--- start-api.sh
+++ start-api.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-python3 -m venv venv
-source venv/bin/activate
+python3 -m venv .venv
+source .venv/bin/activate
 pip install -r requirements.txt  # Install dependencies
-source venv/bin/activate
 python api.py
